4d886e94df5ba880415a802099243c8b81407336
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-03-08 11:45:04 +0100

    Task 8 - n3 student class

---
+++ src/n3/Student.java
@@ -0,0 +1,66 @@
+package n3;
+
+import java.util.Random;
+
+public class Student {
+    private String name;
+    private int age;
+    private String course;
+    private float grade;
+
+    public Student(int seed) {
+        Random rand = new Random(seed);
+
+        String [] names = {"Marc", "Leo", "Alex", "Jan", "Nil",
+                "Julia", "Martina", "Emma", "Lucia", "Ona"};
+        String [] courses = {"Java", "Python", "C++", "C#", "Data Science",
+                "Javascript", "BackEnd", "FullStack", "FrontEnd", "PHP"};
+
+        this.name = names[rand.nextInt(0,10)];
+        this.age = rand.nextInt(18, 100);
+        this.course = courses[rand.nextInt(0,10)];
+        this.grade = rand.nextInt(0, 10);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", course='" + course + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
